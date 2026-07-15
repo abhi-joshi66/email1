97c617cdf1fb98c1283b4ec7649fc894c3beadd4
abhijit <abhijit.joshi@mouser.com>
2026-07-15 17:06:48 +0530

    fix: fix prod env issues

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ require("./routes/authRoutes")(app);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
