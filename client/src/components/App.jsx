c7d96499e1defe33f46105cd1fbdfb25326acd0a
Abhijit Joshi <abhijit.joshi66@gmail.com>
2026-07-18 03:15:19 +0530

    fix changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to the Email Project</h1>
-    </div>
+    <Router>
+      <div>
+        <h1>Welcome to the Email Project</h1>
+      </div>
+    </Router>
   );
 };
 
